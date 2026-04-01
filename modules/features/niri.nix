a994098e2a734f746e10e3cd97e50274713286a9
Connor Simms <connorvsimms@gmail.com>
2026-04-01 02:36:58 +0000

    Unwrapped noctalia and niri

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -5,25 +5,19 @@
   flake.modules.nixos.niri = { pkgs, ... }: {
     programs.niri = {
       enable = true;
-      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
     };
   };
 
-  # User Level (Dotfiles/user-apps related to Niri)
+  # User Level 
   flake.modules.homeManager.niri = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      
-    ];
-  };
+    programs.niri = {
+      enable = true;
 
-  perSystem = { pkgs, lib, self', ... }: {
-    packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
-      inherit pkgs;
       settings = {
         prefer-no-csd = true;
 
         spawn-at-startup = [
-	  (lib.getExe self'.packages.myNoctalia)
+	  lib.getExe pkgs.noctalia-shell
         ];
 
         input = {
@@ -33,22 +27,90 @@
               options = "ctrl:nocaps,altwin:swap_alt_win";
             };
 
-            repeat-delay = 450;
+            repeat-delay = 300;
             repeat-rate = 20;
           };
         };
 
-        outputs."Samsung" = {
-          mode = "5120x1440";
+        # run 'niri msg outputs'
+        outputs."HDMI-A-1" = {
+          mode = "5120x1440@240.000";
+          scale = 1.0;
         };
 
         binds = {
-          "Mod+T".spawn-sh = lib.getExe pkgs.kitty;
-          "Mod+Q".close-window = _: {};
+          # Launchers 
+          "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
+          "Mod+D".spawn-sh = lib.getExe pkgs.rofi;
+          "Mod+B".spawn-sh = lib.getExe pkgs.rofi;
+
+          # Visibility
+          "Mod+Q".close-window = _:{};
+          "Mod+C".center-column = _:{};
+          "Mod+F".maximize-column = _:{};
+
+          # Focus
+          "Mod+H".focus-column-left = _:{};
+          "Mod+J".focus-window-down = _:{};
+          "Mod+K".focus-window-up = _:{};
+          "Mod+L".focus-column-right = _:{};
+
+          "Mod+1".focus-workspace = 1;
+          "Mod+2".focus-workspace = 2;
+          "Mod+3".focus-workspace = 3;
+          "Mod+4".focus-workspace = 4;
+          "Mod+5".focus-workspace = 5;
+
+          "Mod+Shift+1".move-column-to-workspace = 1;
+          "Mod+Shift+2".move-column-to-workspace = 2;
+          "Mod+Shift+3".move-column-to-workspace = 3;
+          "Mod+Shift+4".move-column-to-workspace = 4;
+          "Mod+Shift+5".move-column-to-workspace = 5;
+
+          # Sizing
+          "Mod+R".switch-preset-column-width = _:{};
+          "Mod+Shift+R".switch-preset-window-height = _:{};
+
+          "Mod+Minus".set-column-width = "-10%";
+          "Mod+Equal".set-column-width = "+10%";
+          "Mod+Shift+Minus".set-window-height = "-10%";
+          "Mod+Shift+Equal".set-window-height = "+10%";
         };
 
         layout = {
           gaps = 5;
+
+          default-column-width = {
+            proportion = 0.33333;
+          };
+
+          preset-column-widths = [
+            { proportion = 0.33333; }  
+            { proportion = 0.5; }  
+            { proportion = 0.66667; }  
+            { fixed = 750; }  
+          ];
+
+          preset-window-heights = [
+            { proportion = 0.5; }  
+            { proportion = 0.66667; }  
+            { proportion = 1.0; }  
+            { fixed = 750; }  
+          ];
+
+          focus-ring = {
+            enable = true;
+            width = 2;
+          };
+
+          border = {
+            enable = false;
+            width = 2;
+          };
+
+          insert-hint = {
+            enable = true;
+          };
         };
       };
     };
--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -1,27 +1,10 @@
 { self, inputs, ... }:
 
 {
-  # OS Level
-  flake.modules.nixos.noctalia = { pkgs, ... }: {
+  # User Level 
+  flake.modules.homeManager.noctalia = { pkgs, ... }: {
     programs.noctalia-shell = {
       enable = true;
-      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNoctalia;
-    };
-  };
-
-  # User Level (Dotfiles/user-apps related to Noctalia)
-  flake.modules.homeManager.noctalia = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      
-    ];
-  };
-
-  perSystem = { pkgs, lib, self', ... }: {
-    packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
-      inherit pkgs;
-      settings = {
-
-      };
     };
   };
 }
