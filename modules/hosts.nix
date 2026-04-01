564d2fc68e1a447918796a4113e48936a574ffd2
Connor Simms <connorvsimms@gmail.com>
2026-04-01 18:10:52 +0000

    fixed typo

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -1,10 +1,8 @@
 # defines all hosts + users + homes.
 # then config their aspects in as many files you want
 {
-  # tux user at igloo host.
-  den.hosts.x86_64-linux.igloo.users.csimms = { };
+  den.hosts.x86_64-linux.desktop.users.csimms = { };
 
-  # define an standalone home-manager for tux
   den.homes.x86_64-linux.csimms = { };
 
   # be sure to add nix-darwin input for this:
