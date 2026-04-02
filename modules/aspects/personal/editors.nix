65644cc917bcec2b1c7f6d918f1b90f26fccab42
Connor Simms <connorvsimms@gmail.com>
2026-04-02 18:21:36 +0000

    fix overriding of config

--- modules/aspects/personal/editors.nix
+++ modules/aspects/personal/editors.nix
@@ -2,29 +2,18 @@
 {
   personal.editors = {
     homeManager = { config, pkgs, ... }: {
-      
-      # Vim (No Dependency Config)
-      programs.vim = {
-        enable = true;
-        # defaultEditor = false;
-      };
 
+      home.packages = with pkgs; [
+        vim
+        neovim
+        ripgrep
+        fd
+        fzf
+        rust-analyzer
+      ];
+      
       home.file.".vimrc".source = 
         config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/vim/vimrc";
-
-      # Neovim
-      programs.neovim = {
-        enable = true;
-        # defaultEditor = true;
-
-        extraPackages = with pkgs; [
-          ripgrep   # Telescope fuzzy finding
-          fd        # Telescope file finding
-          gcc       # Compiling Treesitter parsers
-          xclip     # System clipboard support on Linux
-          wl-clipboard # System clipboard support on Wayland
-        ];
-      };
 
       xdg.configFile."nvim".source = 
         config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/nvim";
