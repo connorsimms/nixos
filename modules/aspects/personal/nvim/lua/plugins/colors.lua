0ae6815bded0db929770efe49bd2dd387bb35aa3
connorsimms <connorvsimms@gmail.com>
2026-04-09 21:52:42 -0500

    Trying colors

--- modules/aspects/personal/nvim/lua/plugins/colors.lua
+++ modules/aspects/personal/nvim/lua/plugins/colors.lua
@@ -1,21 +1,30 @@
+vim.g.gruvbox_material_background = 'medium'
+vim.g.gruvbox_material_foreground = 'material'
+vim.g.gruvbox_material_visual = 'grey background'
+vim.g.gruvbox_material_cursor = 'green'
+vim.g.gruvbox_material_disable_italic_comment = 1
+vim.g.gruvbox_material_enable_italic = true
+vim.g.gruvbox_material_statusline_style = 'mix'
+vim.g.gruvbox_material_lightline_disable_bold = 1
+vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_background = 'hard'
 vim.g.gruvbox_material_foreground = 'material'
-
+vim.g.gruvbox_material_enable_italic = true
 vim.cmd.colorscheme('gruvbox-material')
 
-require("transparent").setup({
-  groups = {
-    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-    'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
-    'EndOfBuffer',
-  },
-  enable = true,
-  extra_groups = {},
-  exclude_groups = {},
-  on_clear = function() end,
-})
+-- require("transparent").setup({
+--   groups = {
+--     'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+--     'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+--     'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+--     'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+--     'EndOfBuffer',
+--   },
+--   enable = true,
+--   extra_groups = {},
+--   exclude_groups = {},
+--   on_clear = function() end,
+-- })
 
 require("lualine").setup({
   options = { theme = 'gruvbox-material' }
--- modules/aspects/personal/nvim/lua/plugins/lsp.lua
+++ modules/aspects/personal/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,5 @@
 local configs = require('lspconfig.configs')
 
--- 1. Define your shared "on_attach" logic
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local opts = { buffer = args.buf }
@@ -11,14 +10,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
--- 2. Use the new 0.12 syntax to enable servers
--- This tells Neovim: "Use the industrial settings from lspconfig for these"
 vim.lsp.enable('nil_ls')
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('clangd')
 vim.lsp.enable('pyright')
 
--- 3. If you need specific settings (like for Lua), do it here
 vim.lsp.config('lua_ls', {
   settings = {
     Lua = {
