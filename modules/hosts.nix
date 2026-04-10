9770a157d10fe813c869c1b88dcd6145d0243219
connorsimms <connorvsimms@gmail.com>
2026-04-09 21:03:13 -0500

    Nevermind

--- modules/hosts.nix
+++ modules/hosts.nix
@@ -2,6 +2,7 @@
 {
   den.hosts.x86_64-linux.desktop.users.csimms = { };
   den.hosts.aarch64-darwin.macbook.users.csimms = { };
+
   den.homes.x86_64-linux.csimms = { };
   den.homes.aarch64-darwin.csimms = { };
 }
