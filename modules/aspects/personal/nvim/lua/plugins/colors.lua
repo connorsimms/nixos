d0a9e2a12d3fe53496a798046fd862e6d7df0ef2
connorsimms <connorvsimms@gmail.com>
2026-04-10 17:31:48 -0500

    Fine

--- modules/aspects/personal/nvim/lua/plugins/colors.lua
+++ modules/aspects/personal/nvim/lua/plugins/colors.lua
@@ -1,15 +1,5 @@
-vim.g.gruvbox_material_background = 'medium'
+vim.g.gruvbox_material_background = 'soft'
 vim.g.gruvbox_material_foreground = 'material'
-vim.g.gruvbox_material_visual = 'grey background'
-vim.g.gruvbox_material_cursor = 'green'
-vim.g.gruvbox_material_disable_italic_comment = 1
-vim.g.gruvbox_material_enable_italic = true
-vim.g.gruvbox_material_statusline_style = 'mix'
-vim.g.gruvbox_material_lightline_disable_bold = 1
-vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_background = 'hard'
-vim.g.gruvbox_material_foreground = 'material'
-vim.g.gruvbox_material_enable_italic = true
 vim.cmd.colorscheme('gruvbox-material')
 
 -- require("transparent").setup({
